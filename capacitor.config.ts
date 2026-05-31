0d0c8bad3b17fa7814049c3876ef1b9d5eda71fd
Radu Slavila <radu.slavila91@gmail.com>
2026-05-31 12:33:11 +0300

    feat: initialize React project with Vite, add SVG assets and global styles
    - Added React and Vite setup with main entry point in main.tsx
    - Created global styles in index.css with light and dark theme support
    - Included SVG assets for React and Vite logos
    - Configured TypeScript with appropriate settings in tsconfig files
    - Set up Vite configuration for React plugin

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ro.slavila.sudokuapp',
+  appName: 'Sudoku Synapse',
+  webDir: 'dist'
+};
+
+export default config;
